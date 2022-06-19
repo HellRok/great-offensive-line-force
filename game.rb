85b26e8dbb5e44f8ea3b3c1c9dde807a7c71f939
Sean Earle <sean.r.earle@gmail.com>
2022-06-19 22:19:38 +0930

    Initial commit with tilemap

---
+++ game.rb
@@ -0,0 +1,39 @@
+# Add the path ./vendor so we can easily require third party libraries.
+$: << './vendor'
+
+# Open up a window
+init_window(800, 480, "golf")
+
+# Setup audio so we can play sounds
+init_audio_device
+
+# Get the current monitor frame rate and set our target framerate to match.
+set_target_fps(get_monitor_refresh_rate(get_current_monitor))
+
+# Define your main method
+def main
+  # Get the amount of time passed since the last frame was rendered
+  delta = get_frame_time
+
+  # Your update logic goes here
+
+  drawing do
+    # Your drawing logic goes here
+
+    clear
+    draw_text(
+      "Welcome to your first Taylor application!",
+      190, 200, 20, DARKGRAY
+    )
+  end
+end
+
+if browser?
+  set_main_loop 'main'
+else
+  # Detect window close button or ESC key
+  main until window_should_close?
+end
+
+close_audio_device
+close_window
